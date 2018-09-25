4e46e112a0dbfb80d9d4cab0717ba403d0a4eb67
codemac <codemac-01i@codemacs-Mac-mini.local>
2018-09-25 16:25:05 +0530

    adding all folders

---
+++ MealsLikeMagic/fgf.swift
@@ -0,0 +1,9 @@
+//
+//  fgf.swift
+//  MealsLikeMagic
+//
+//  Created by codemac on 9/25/18.
+//  Copyright © 2018 codemac. All rights reserved.
+//
+
+import Foundation
---
+++ MealsLikeMagic/gfgd.swift
@@ -0,0 +1,9 @@
+//
+//  gfgd.swift
+//  MealsLikeMagic
+//
+//  Created by codemac on 9/25/18.
+//  Copyright © 2018 codemac. All rights reserved.
+//
+
+import Foundation
